09158dae2ffe27ac884df31311d5e8756d354f4e
Stephen Ciauri <stephen.ciauri@gmail.com>
2016-04-18 18:33:38 -0700

    Added proper pictures.

--- Manners/Constants.swift
+++ Manners/Constants.swift
@@ -13,21 +13,27 @@ struct Constants{
     static let topics: [[String:AnyObject]] = [
     ["title":"Please and Thank You",
         "article": "Be nice to people. When you have spent time at a friend's house, remember to thank your hosts for having you over and for the good time you had.",
-        "image": "lolk"],
+        "image": "thanks"],
     ["title":"Interrupting",
         "article": "Do not interrupt people who are speaking with each other unless there is an emergency. They will notice you and respond when they are finished talking. If you do need to get somebody's attention right away, the phrase \"excuse me\" is the most polite way for you to enter the conversation.",
-        "image":"lolk"],
+        "image":"rude"],
     ["title":"Permission",
         "article": "When you have any doubt about doing something, ask permission first. It can save you from many hours of grief later.",
-        "image": "lolk"],
+        "image": "permission"],
     ["title":"Opinions",
         "article": "Keep negative opinions to yourself, or between you and your friends, and out of earshot of others. Do not comment on other people's physical characteristics unless, of course, it's to compliment them, which is always welcome.",
-        "image": "lolk"],
+        "image": "opinion"],
     ["title":"Phone Calls",
         "article": "When you make a phone call, introduce yourself first and then ask if you can speak with the person you are calling. Use a quiet voice inside, especially in places like restaurants. Never use foul language in front of people if you don't know if it will bother them.",
-        "image": "lolk"],
+        "image": "phone"],
     ["title":"Crowds",
         "article": "Do not make fun of anyone for any reason. Teasing shows others you are weak, and ganging up on someone else is cruel. Even if an event is boring, sit through it quietly and pretend that you are interested. The performers and presenters are doing their best.  If you bump into somebody, immediately say \"Excuse me.\"",
-        "image": "lolk"]
+        "image": "crowd"],
+    ["title":"Nose Picking",
+    "article": "Don't do it in public. You should usually use a tissue. Definitly don't eat the boogers.",
+        "image": "nose"],
+    ["title":"Private Parts",
+        "article": "Don't touch your private parts in public. Don't touch anyone elses private parts either. That is why they are private.",
+        "image": "privates"]
     ]
 }
